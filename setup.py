6cc67ae9fa7aa898518f410abc761298f54a879d
Andrew Martin <a.a.d.martin@gmail.com>
2016-08-22 15:10:12 +1000

    minor cosmetic changes

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ install_requires = [
 
 setup(
     name="ha-release",
-    version="0.6.2",
+    version="0.6.3",
     description="Phase new EC2 Instances into an AWS Autoscaling Group without Downtime",
     license="MIT",
     author="adamar",
